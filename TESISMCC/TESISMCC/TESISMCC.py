5d15c887056d0af3269b8a6784554b4ff5b96347
Oscar Daniel <oscar.atnc@gmail.com>
2017-11-08 12:41:56 -0700

    c

--- TESISMCC/TESISMCC/TESISMCC.py
+++ TESISMCC/TESISMCC/TESISMCC.py
@@ -1,1 +1,42 @@
+import MAX3010X as MAX30102
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+max102 = MAX30102.MAX30102.max102
+GPIO.setup(4, GPIO.IN)
+int_status = GPIO.input(4)
+i = 0
+Red = []
+IR = []
+
+print "interrupt status", int_status
+
+Spo2Sensor = MAX30102.MAX30102()
+Spo2Sensor.begintest(Spo2Sensor.MAX30102_PARTID, Spo2Sensor.MAX30102_EXPECTED_PARTID)
+Spo2Sensor.setup(31, 4, 2, 100, 411, 4096)
+print"###################################################."
+Spo2Sensor.check()
+
+
+
+
+
+
+
+#while i < 10:
+ #   print "read pointer 1: ", Spo2Sensor.getReadPointer()
+  #  sample = max102.read_i2c_block_data(Spo2Sensor.MAX30102_ADDRESS, Spo2Sensor.MAX30102_FIFODATAREG,6)
+  # print "read pointer 2: ", Spo2Sensor.getReadPointer()
+  #  print "sample read:", sample
+  #  Redsample = sample[0:3]
+  #  IRsample = sample[3:6]
+  #  Redvalue = Red.append(Spo2Sensor.concatbyte(Redsample))
+  #  IRvalue  = IR.append(Spo2Sensor.concatbyte(IRsample))
+  #  print Red
+  #  print IR
+  #  i = i+1
+
+
+
+
 
