db5dfd830935db341a37b18be775221435bcac59
Oscar Daniel <oscar.atnc@gmail.com>
2017-11-12 17:46:49 -0700

    -

---
+++ TESISMCC/TESISMCC/HeartRate.py
@@ -0,0 +1,17 @@
+
+IR_AC_Max = 20
+IR_AC_Min = -20
+
+IR_AC_Signal_Current = 0
+IR_AC_Signal_Previous
+IR_AC_Signal_min = 0
+IR_AC_Signal_max = 0
+IR_Average_Estimated
+
+positiveEdge = 0
+negativeEdge = 0
+ir_avg_reg = 0
+
+cbuf=[0]*32
+offset = 0;
+FIRCoeffs = {172, 321, 579, 927, 1360, 1858, 2390, 2916, 3391, 3768, 4012, 4096}
