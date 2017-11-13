bd13e1e3dadfacb2c3d305d2f513772c2ae5feb4
Oscar Daniel <oscar.atnc@gmail.com>
2017-11-12 19:27:19 -0700

    .

--- TESISMCC/TESISMCC/TESISMCC.py
+++ TESISMCC/TESISMCC/TESISMCC.py
@@ -23,12 +23,12 @@ Spo2Sensor = MAX30102.MAX30102()
 Spo2Sensor.begintest(Spo2Sensor.MAX30102_PARTID, Spo2Sensor.MAX30102_EXPECTED_PARTID)
 Spo2Sensor.setup(31, 4, 2, 100, 411, 4096)
 
-
-while (1):
+i=0
+while i in range (30):
     
     reD = Spo2Sensor.lastCorrect(Spo2Sensor.getRed())
     iR  = Spo2Sensor.lastCorrect(Spo2Sensor.getIR())
-    
+    i+=1 
     print "R: ",reD , "IR: ", iR
 
 
