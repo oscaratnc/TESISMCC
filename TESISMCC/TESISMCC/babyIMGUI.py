7e8d009365f7c76d8bbde517373006d09d227b60
Oscar Daniel <oscar.atnc@gmail.com>
2017-11-19 21:06:06 -0700

    .

--- TESISMCC/TESISMCC/babyIMGUI.py
+++ TESISMCC/TESISMCC/babyIMGUI.py
@@ -1,14 +1,20 @@
 import sys
 from PyQt4 import QtGui
 
+class babyIMGUI (QtGui.QWidget):
+    def __init__():
+        super(babyIMGUI, self).__init__()
+        self.iniUI()
+    
+    def initUI(self):
+        self.setGeometry(600,300,400,200)
+        self.setWindowTitle('New GUI')
+        self.show()
+
+
 def main():
     app = QtGui.QApplication(sys.argv)
-    w = QtGui.QWidget()
-    w.resize(400,200)
-    w.move(600,300)
-    w.setWindowTitle('BabyIM')
-    w.show()
-
+    w = babyIMGUI()
     app.exec_()
 
 if __name__ == '__main__' :
