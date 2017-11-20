606b4051283f29fc9c1d15f70cfbeda5fdd918c9
Oscar Daniel <oscar.atnc@gmail.com>
2017-11-20 00:06:46 -0700

    .

--- TESISMCC/TESISMCC/babyIMGUI.py
+++ TESISMCC/TESISMCC/babyIMGUI.py
@@ -13,20 +13,12 @@ class babyIMGUI (QtGui.QWidget):
         self.btn = QtGui.QPushButton('Button', self)
         self.btn.resize(self.btn.sizeHint())
         self.btn.move(150,100)
-        self.btn.clicked.connect(self.signalDialog)
+       
 
-        self.lbl = QtGui.QLabel(self)
-        self.lbl.move(180,50)
-        self.lbl.setText('Slot')
-        self.lbl.adjustSize()
+       
 
         self.show()
-    def signalDialog(self):
-       text, ok = QtGui.QInputDialog.getText(self, 'Dialog Box', 'Enter text: ')
-       if ok:
-           self.lbl.setText(str(text))
-           self.lbl.adjustSize()
-
+   
 def main():
     app = QtGui.QApplication(sys.argv)
     w = babyIMGUI()
