0c5bef2ad69ba82ac784dc5b6cebc52385f96ef7
Oscar Daniel <oscar.atnc@gmail.com>
2017-11-19 18:40:12 -0700

    .

--- TESISMCC/TESISMCC/GuiTest.py
+++ TESISMCC/TESISMCC/GuiTest.py
@@ -1,18 +1,23 @@
 import sys
-from PyQt4 import QtGui
-
-import sys
-from PyQt4 import QtGui
+from PyQt4 import QtGui, QtCore
 
 class  Window (QtGui.QMainWindow):
     def __init__(self):
         super(Window, self).__init__()
         self.setGeometry(50,50,500,300)
         self.setWindowTitle("PyQt tuts!")
-        self.show()
+        self.home()
 
+    def home(self):
+        btn = QtGui.QPushButton("Quit",self)
+        btn.clicked.connect(QtCore.QCoreApplication.instance().quit)
+        btn.resize(100,100)
+        btn.move(100,100)
+        sekf.show()
 
-app= QtGui.QApplication(sys.argv)
-GUI=Window()
-sys.exit(app.exec_())
+    def run():
+        app= QtGui.QApplication(sys.argv)
+        GUI=Window()
+        sys.exit(app.exec_())
+run()
 
