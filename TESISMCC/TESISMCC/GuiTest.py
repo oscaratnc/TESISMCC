5194e071a017736eaafbc727d3ab3f9f5993dd7a
Oscar Daniel <oscar.atnc@gmail.com>
2017-11-19 19:17:06 -0700

    z

--- TESISMCC/TESISMCC/GuiTest.py
+++ TESISMCC/TESISMCC/GuiTest.py
@@ -6,13 +6,24 @@ class  Window (QtGui.QMainWindow):
         super(Window, self).__init__()
         self.setGeometry(50,50,500,300)
         self.setWindowTitle("PyQt tuts!")
+        extractAction = QtGui.QAction("&HIIII",self)
+        extractAction.setShortcut("Ctrl+Q")
+        extractAction.setStatusTip('Leave the App')
+        extractAction.triggered.connect(self.close_application)
+
+        self.statusBar()
+
+        mainMenu = self.menuBar()
+        fileMenu = mainMenu.addMenu('&FIle')
+        fileMenu.addAction(extractAction)
+
         self.home()
 
     def home(self):
         btn = QtGui.QPushButton("Quit",self)
         btn.clicked.connect(QtCore.QCoreApplication.instance().quit)
         btn.resize(btn.sizeHint())
-        btn.move(0,0)
+        btn.move(100,100)
         self.show()
 
     def close_application():
