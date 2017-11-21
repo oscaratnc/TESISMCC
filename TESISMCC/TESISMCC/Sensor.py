3110b5c3f8500d7ecf85f908accb99fb6e950afe
Oscar Daniel <oscar.atnc@gmail.com>
2017-11-21 15:16:12 -0700

    .

--- TESISMCC/TESISMCC/Sensor.py
+++ TESISMCC/TESISMCC/Sensor.py
@@ -27,8 +27,9 @@ IR = np.array([])
 def getECG(self, numSeconds):
     startTime = wiringpi.millis()
     while wiringpi.millis()-startTime < (numSeconds*1000): 
+        print wiringpi.milis()
         Ecg = round((mcp.read_adc(1)*3.3)/1024,3)
-        self.ecgValue = np.append(self.ecgValues,Ecg)
+        self.ecgValues = np.append(self.ecgValues,Ecg)
         wiringpi.delayMicroseconds(400)
         
 
@@ -36,6 +37,7 @@ def getSpo2(self,numSeconds):
     
    startTime = wiringpi.millis()
    while wiringpi.millis()-startTime < (numSeconds*1000): 
+       print wiringpi.millis()
        reD = Spo2Sensor.getRed()
        iR  = Spo2Sensor.getIR()
        #print "R: ", reD , "IR: ", iR
