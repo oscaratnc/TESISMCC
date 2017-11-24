4aaa562bf8ab0dc396ef02205256c737c49e5eb2
Oscar Daniel <oscar.atnc@gmail.com>
2017-11-24 15:56:11 -0700

    .

--- TESISMCC/TESISMCC/babyIMGUI.py
+++ TESISMCC/TESISMCC/babyIMGUI.py
@@ -3,8 +3,8 @@ from pyqtgraph.Qt import QtGui, QtCore
 import numpy as np
 import pyqtgraph as pg
 
-numSecondsECG=3
-numSecondsSpO2=3
+numSecondsECG=5
+numSecondsSpO2=5
 Sensors.beginSpO2(Sensors,200)
 Sensors.getECG(Sensors,numSecondsECG)
 print "ECG done"
