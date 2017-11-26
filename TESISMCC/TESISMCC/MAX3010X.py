d21e6f6d76697651df89c0a7d1dbc8f44f9f7ca1
Oscar Daniel <oscar.atnc@gmail.com>
2017-11-26 01:04:45 -0700

    -

--- TESISMCC/TESISMCC/MAX3010X.py
+++ TESISMCC/TESISMCC/MAX3010X.py
@@ -726,7 +726,7 @@ class MAX30102:
         nyquist = 0.5 * sampleF
         Wn = fc/nyquist
         b, a = sp.butter(order,Wn,'low',False,'ba')
-        filtered  = sp.lfilter(a,b,signal)
+        filtered  = sp.lfilter(b,a,signal)
         return filtered
         
 
