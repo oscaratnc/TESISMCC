bd9a8913f0adbf4169c0e62683c02561e425cbe8
Oscar Daniel <oscar.atnc@gmail.com>
2017-11-27 13:45:07 -0700

    v

--- TESISMCC/TESISMCC/Sensor.py
+++ TESISMCC/TESISMCC/Sensor.py
@@ -54,12 +54,12 @@ def getSpo2(self,numSeconds):
        #print "R: ", reD , "IR: ", iR
        self.Red = np.append(self.Red,reD)  
        self.IR = np.append(self.IR,iR)
-   self.RedAC = Spo2Sensor.lowPasFilter(self.Red,6,100)
    self.RedAC = Spo2Sensor.removeDC(self.Red)
+   self.RedAC = Spo2Sensor.lowPasFilter(self.Red,6,100)
    
-   self.IRAC = Spo2Sensor.lowPasFilter(self.IR,6,100)
    self.IRAC = Spo2Sensor.removeDC(self.IR)
-
+   self.IRAC = Spo2Sensor.lowPasFilter(self.IR,6,100)
+   
    
 
    print "min IR:", min(self.IR)
