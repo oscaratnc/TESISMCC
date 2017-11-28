9a244ae4d680d01f8b1a4db733eadec072e24f09
Oscar Daniel <oscar.atnc@gmail.com>
2017-11-28 16:30:12 -0700

    b

--- TESISMCC/TESISMCC/babyIMGUI.py
+++ TESISMCC/TESISMCC/babyIMGUI.py
@@ -23,13 +23,13 @@ print IR
 
 IRFile = open("IRfile.txt", 'w')
 IRFile.write("Red:\n ")
-for i in np.nditer(RED):
-    valueRed = str(i)
+for i in range(RED.shape(0)):
+    valueRed = str(RED(i))
     IRFile.write(valueRed)
 
 IRFile.write("IR: ")
-for i in np.nditer(IR):
-    valueIR = str(i)
+for i in range(IR.shape(0)):
+    valueIR = str(IR(i))
     IRFile.write(valueIR)
     
 IRFile.close
