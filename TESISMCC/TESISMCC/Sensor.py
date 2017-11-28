1949442ea50db5cf2103e213e168c5f20e81a88a
Oscar Daniel <oscar.atnc@gmail.com>
2017-11-27 18:32:26 -0700

    .

--- TESISMCC/TESISMCC/Sensor.py
+++ TESISMCC/TESISMCC/Sensor.py
@@ -61,10 +61,10 @@ def getSpo2(self,numSeconds):
        self.Red = np.append(self.Red,reD)  
        self.IR = np.append(self.IR,iR)
    #self.Red = Spo2Sensor.removeDC(self.Red)
-   self.Red = Spo2Sensor.lowPasFilter(self.Red,10,samplerate)
+   self.Red = Spo2Sensor.lowPasFilter(self.Red,6,samplerate)
    
    #self.IR = Spo2Sensor.removeDC(self.IR)
-   self.IR = Spo2Sensor.lowPasFilter(self.IR,10,samplerate)
+   self.IR = Spo2Sensor.lowPasFilter(self.IR,6,samplerate)
    
    
 
