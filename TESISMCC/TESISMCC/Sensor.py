0c613fab626878537cfe0bfe772efe8d513ba0be
Oscar Daniel <oscar.atnc@gmail.com>
2017-11-30 13:47:10 -0700

    .

--- TESISMCC/TESISMCC/Sensor.py
+++ TESISMCC/TESISMCC/Sensor.py
@@ -58,8 +58,8 @@ def getSpo2(self, numSeconds):
          mx102.read_sensor()
     Red = mx102.buffer_red
     IR = mx102.buffer_ir
-    print red
-    print iR
+    print Red
+    print IR
     #      # print (wiringpi.millis()-startTime)/1000
     #       reD = Spo2Sensor.getRed()
     #       iR  = Spo2Sensor.getIR()
