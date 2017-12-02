a04c181a0535954ddce2693f6522d9c4d7815cde
Oscar Daniel <oscar.atnc@gmail.com>
2017-12-02 15:42:18 -0700

    .

--- TESISMCC/TESISMCC/Sensor.py
+++ TESISMCC/TESISMCC/Sensor.py
@@ -64,7 +64,7 @@ def getSpo2(self,numSeconds):
    
     print "Samples = ",numberSamples
     while i <= numberSamples:
-        print Sp2.Spo2Sensor.i2c.read_byte_data(Spo2.ADDRESS,Spo2.FIFODATAREG)
+        print Sp2.Spo2Sensor.i2c.read_i2c_block_data(Spo2.ADDRESS,Spo2.FIFODATAREG,6)
         numberSamples = Spo2.getNumberofSamples()
 
         WP  = Spo2.getWritePointer()
