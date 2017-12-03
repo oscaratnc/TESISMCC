88dfee86d8e22a9d2d23e2745c036f04c6c7ca53
Oscar Daniel <oscar.atnc@gmail.com>
2017-12-03 10:36:23 -0700

    .

--- TESISMCC/TESISMCC/babyIMGUI.py
+++ TESISMCC/TESISMCC/babyIMGUI.py
@@ -30,16 +30,16 @@ win.setWindowTitle("Signals Ploting")
 
 pg.setConfigOptions(antialias= True)
 
-#p1 = win.addPlot(title="ECG")
+p1 = win.addPlot(title="ECG")
 p1.plot(ECG, pen=(255,0,0))
 
 win.nextRow()
-#p2 = win.addPlot(title = "RED LED")
+p2 = win.addPlot(title = "RED LED")
 p2.plot(RED, pen=(0,255,0))
 
-#win.nextRow()
-#p3= win.addPlot(title = "IR LED")
-p2.plot(IR, pen=(0,0,255))
+win.nextRow()
+p3= win.addPlot(title = "IR LED")
+p3.plot(IR, pen=(0,0,255))
 
 if __name__ == '__main__':
     import sys
