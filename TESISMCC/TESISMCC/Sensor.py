bb03ce89b52084b97d0d502223e90f1e232ffb02
Oscar Daniel <oscar.atnc@gmail.com>
2017-12-03 17:45:56 -0700

    .

--- TESISMCC/TESISMCC/Sensor.py
+++ TESISMCC/TESISMCC/Sensor.py
@@ -58,7 +58,7 @@ def getSpo2(self,numSeconds, samplerate):
     Spo2.setFIFOAF(AFthreshold)
     interrupt  = Button(7)
     i = 0
-    while wiringpi.millis()-startTime < numSeconds:
+    while wiringpi.millis()-startTime < numSeconds*1000:
        #interrupt.when_activated = Spo2.readSample()
        interrupt.when_activated = Spo2.ReadFIFOFULL()
        i+=1
