53fbee348b2f15a89bf339c6969a3d7cef254595
Oscar Daniel <oscar.atnc@gmail.com>
2017-12-04 17:57:58 -0700

    0

--- TESISMCC/TESISMCC/babyIMGUI.py
+++ TESISMCC/TESISMCC/babyIMGUI.py
@@ -29,7 +29,7 @@ win.resize(500,500)
 win.setWindowTitle("Signals Ploting")
 
 pg.setConfigOptions(antialias= True)
-pen1 = pg.mkPen((255,0,0),6)
+pen1 = pg.mkPen(pg.mkColor(255,0,0),width= 6)
 p1 = win.addPlot(title="ECG")
 p1.plot(ECG, pen1)
 
