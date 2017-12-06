df4c8122c9167b6ab818553dd77638509f083c52
Oscar Daniel <oscar.atnc@gmail.com>
2017-12-05 17:27:19 -0700

    .

--- TESISMCC/TESISMCC/Processing.py
+++ TESISMCC/TESISMCC/Processing.py
@@ -17,7 +17,7 @@ class Processing:
         nyqRate = sampleF/2.0
         f1 = (fc-20)/nyqRate
         f2 = (fc+20)/nyqRate
-        order = 20 
+        order = 25
         a = sp.firwin(order, [f1,f2])
         Filtered = sp.lfilter(a,1.0,signal)
         return Filtered
